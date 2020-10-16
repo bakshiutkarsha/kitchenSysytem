6d435d3f6915344fce737dc200a8d9cf9443a360
utkarshabakshi <utkarsha.bakshi@gmail.com>
2020-10-16 19:07:29 -0400

    valid url for pdf getting returned

--- pages/barcode.tsx
+++ pages/barcode.tsx
@@ -2,12 +2,31 @@ import * as React from "react";
 import  '../styles/styles.globals.scss';
 //@ts-ignore
 import BarcodeStyles from "../styles/Barcode.scss";
+//@ts-ignore
 import Barcodes from '../src/components/Barcode/index';
 
 
-const BarcodePage: React.FC = ( ) => {
+const BarcodePage: React.FC <any>= () => {
+  const [pdfLink, serPdfLink] = React.useState('');
+
+  const [loader, setLoader] = React.useState(false)
+
+  const getPdfLink = async() => {
+      setLoader(true);
+      const res = await fetch('http://localhost:5000/barcodes.pdf');
+      serPdfLink(await res.json());
+      setLoader(false)
+  }
+  
+
   return <div className={BarcodeStyles.barcodeCntr}>
       <Barcodes></Barcodes>
+      <div className={BarcodeStyles.barcodeButton} onClick={getPdfLink}><span>Get PDF Link 
+        {loader ? <img className={BarcodeStyles.loader} src="/loader.gif"/> : ''}
+        </span></div>
+       <div className={BarcodeStyles.urlCntr}>URL:
+          {pdfLink ? <div>{pdfLink}</div>:''}
+       </div>
   </div>
   
 };
